b7eb33ee17d1637a408831a7878c3218d3d8dfad
Joao keuwe <joaov2872@hotmail.com>
2024-02-16 21:20:06 -0300

    feat:

---
+++ src/tabs/Main.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { VStack, Text } from "native-base";
+
+const Main = () => {
+  return (
+    <VStack>
+      <Text>Main</Text>
+    </VStack>
+  )
+};
+
+export default Main;
